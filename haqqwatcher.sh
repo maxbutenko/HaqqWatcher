93557eb75082f09b31c902101aa9afdf6e79f999
ReBoot <81085478+maxbutenko@users.noreply.github.com>
2022-10-11 17:44:41 +0400

    Update haqqwatcher.sh
    path to binary fixed

--- haqqwatcher.sh
+++ haqqwatcher.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 #variables
 source /root/haqqwatcher/.env
 freeMem=$(free -g |tail -n2|awk '{print $6}')
 let "freeDisk = $(df /|tail -n1|awk '{print $4}')/1048576"
-missedBlocks=$(haqqd q slashing signing-info $(haqqd tendermint show-validator) -o json |jq .missed_block_counter)
+missedBlocks=$($HAQQD q slashing signing-info $($HAQQD tendermint show-validator) -o json |jq .missed_block_counter)
 
 #some message templates
 msgJailed="⚡HAQQ WATCHER alert ⚡ [$MONIKER] in JAIL! %0A [$IP] %0A $VALOPER%0A Command to unjail: %0A haqqd tx slashing unjail --from wallet --chain-id haqq_54211-2"
@@ -13,14 +13,16 @@ msgDiskCritical="⚡ HAQQ WATCHER alert ⚡️ [$MONIKER] The disk is almost ful
 msgMem="⚠️ HAQQ WATCHER info ⚠️ [$MONIKER] Low free RAM! %0A Only $freeMem[G] free. %0A [$IP]"
 
 #Jail check
-jail=$(haqqd query staking validator $VALOPER -o json | jq .jailed)
+jail=$($HAQQD query staking validator $VALOPER -o json | jq .jailed)
 if $jail; then
  if ! $(test -f /root/haqqwatcher/.jail); then
+      echo "file not found, send mess and create file"
       curl -s -X POST $TG_URL -d chat_id=$TG_CHAT_ID -d text="$msgJailed"
       touch /root/haqqwatcher/.jail
  fi
 else
 if $(test -f /root/haqqwatcher/.jail); then
+     echo "File lock found but not in jail "
      curl -s -X POST $TG_URL -d chat_id=$TG_CHAT_ID -d text="$msgUnjailed"
      rm /root/haqqwatcher/.jail
   fi
@@ -28,9 +30,9 @@ fi
 
 #Proposal check
 voteChecked=$(cat $HOME/haqqwatcher/.proposal)
-voteLastNumber=$(haqqd q gov proposals|grep proposal_id|tail  -n 1| awk '{print $2}' | tr -d \")
-voteLastDescription=$(haqqd q gov proposals|grep description|tail -n 1)
-votelastTitle=$(haqqd q gov proposals|grep title|tail -n 1)
+voteLastNumber=$($HAQQD q gov proposals|grep proposal_id|tail  -n 1| awk '{print $2}' | tr -d \")
+voteLastDescription=$($HAQQD q gov proposals|grep description|tail -n 1)
+votelastTitle=$($HAQQD q gov proposals|grep title|tail -n 1)
 if test -f $HOME/haqqwatcher/.proposal; then
 if [ "$voteLastNumber" -gt "$voteChecked" ]; then
 msgProposal="✅ HAQQ WATCHER info ✅ %0A New proposal $voteLastNumber  %0A$votelastTitle  %0A$voteLastDescription %0A Example command to vote: haqqd tx gov vote $voteLastNumber yes --from wallet --fees 500aISLM"
@@ -40,7 +42,7 @@ fi
 fi
 
 #Missed block check
-missedBlocks=$(haqqd q slashing signing-info $(haqqd tendermint show-validator) -o json |jq .missed_block_counter)
+missedBlocks=$($HAQQD q slashing signing-info $($HAQQD tendermint show-validator) -o json |jq .missed_block_counter)
 if  [ "$missedBlocks" != "null" ] ; then
     if [ "$missedBlocks" -ge "MISSED_ALERT" ]; then
        if ! $(test -f /root/haqqwatcher/.miss); then
